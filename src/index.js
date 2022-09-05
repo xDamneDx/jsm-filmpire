b111f5454d92bdfc05cdef86c428ad90ad67362d
xDamneDx <xdamnedx@gmail.com>
2022-09-05 16:43:13 +0300

    Added ThemeProvider & created theme

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
 import App from './components/App';
 
+const theme = createTheme({});
+
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <ThemeProvider theme={theme}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </ThemeProvider>,
 );
