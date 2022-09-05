c4d6cc2753c9343e005622222854f41e56b38e60
xDamneDx <xdamnedx@gmail.com>
2022-09-05 11:21:52 +0300

    Added BrowserRouter

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+);
