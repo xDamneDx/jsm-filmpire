77baa1c81686fc1f7701e33852c7553f3bad469f
xDamneDx <xdamnedx@gmail.com>
2022-09-05 11:46:08 +0300

    Added custom styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
+import useStyles from './styles';
 
-const app = () => (
-  <div>
-    <NavBar />
-    <main>
-      <Routes>
-        <Route path="movie/:id" element={<MovieInformation />} />
-        <Route path="actors/:id" element={<Actors />} />
-        <Route path="profile/:id" element={<Profile />} />
-        <Route path="/" element={<Movies />} />
-      </Routes>
-    </main>
-  </div>
-);
+const app = () => {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Routes>
+          <Route path="movie/:id" element={<MovieInformation />} />
+          <Route path="actors/:id" element={<Actors />} />
+          <Route path="profile/:id" element={<Profile />} />
+          <Route path="/" element={<Movies />} />
+        </Routes>
+      </main>
+    </div>
+  );
+};
 
 export default app;
