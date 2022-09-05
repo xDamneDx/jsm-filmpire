80165227875ae1b665c7ee24496870b4ae6d30b7
xDamneDx <xdamnedx@gmail.com>
2022-09-05 11:23:39 +0300

    Added routes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { Actors, MovieInformation, Movies, NavBar, Profile } from '.';
 
 const app = () => (
-  <div>App</div>
+  <div>
+    <NavBar />
+    <main>
+      <Routes>
+        <Route path="movie/:id" element={<MovieInformation />} />
+        <Route path="actors/:id" element={<Actors />} />
+        <Route path="profile/:id" element={<Profile />} />
+        <Route path="/" element={<Movies />} />
+      </Routes>
+    </main>
+  </div>
 );
 
 export default app;
