49d688246931b87ac7124c2de8b178bba40e36c8
xDamneDx <xdamnedx@gmail.com>
2022-09-06 15:10:11 +0300

    Added redux provider

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { Provider } from 'react-redux';
+import store from './app/store';
 import App from './components/App';
 
 const theme = createTheme({});
 
 createRoot(document.getElementById('root')).render(
-  <ThemeProvider theme={theme}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
+  </Provider>,
 );
