24340a61b085e2380a4d5c0ae7f41d28a1b070a5
xDamneDx <xdamnedx@gmail.com>
2022-09-12 20:58:54 +0300

    Added query for getting movies by search

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -12,7 +12,12 @@ export const tmdbApi = createApi({
     }),
     // Get Movies by [Type]:
     getMovies: builder.query({
-      query: ({ genreIdOrCategoryName, page }) => {
+      query: ({ genreIdOrCategoryName, page, searchQuery }) => {
+        // Get Movies by Search:
+        if (searchQuery) {
+          return `/search/movie?query=${searchQuery}&page=${page}&api_key=${tmdbApiKey}`;
+        }
+
         // Get Movies by Category:
         if (
           genreIdOrCategoryName
