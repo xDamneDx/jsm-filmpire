afea5668ca060f6ba0fa0708998aa70a8f4845f1
xDamneDx <xdamnedx@gmail.com>
2022-09-19 11:48:45 +0300

    Created getRecommendations query

--- src/services/TMDB.js
+++ src/services/TMDB.js
@@ -42,7 +42,16 @@ export const tmdbApi = createApi({
     getMovie: builder.query({
       query: (id) => `/movie/${id}?append_to_response=videos,credits&api_key=${tmdbApiKey}`,
     }),
+    // Get User Specific Lists:
+    getRecommendations: builder.query({
+      query: ({ movie_id, list }) => `movie/${movie_id}/${list}?api_key=${tmdbApiKey}`,
+    }),
   }),
 });
 
-export const { useGetGenresQuery, useGetMoviesQuery, useGetMovieQuery } = tmdbApi;
+export const {
+  useGetGenresQuery,
+  useGetMoviesQuery,
+  useGetMovieQuery,
+  useGetRecommendationsQuery,
+} = tmdbApi;
